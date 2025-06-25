df17e306049ed94469133810c6df1cc6433b88d0
Ravi Malvia <ravi.malvia@clouddrove.com>
2025-06-25 23:24:24 +0530

    fix: Update the variables with its type and description

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,46 @@
-##-----------------------------------------------------------------------------
-## Outputs
-##-----------------------------------------------------------------------------
-output "label_order" {
-  value       = local.label_order
-  description = "Label order."
+output "firewall_id" {
+  description = "Firewall ID"
+  value       = join("", azurerm_firewall.firewall.*.id)
+}
+
+output "firewall_name" {
+  value       = join("", azurerm_firewall.firewall.*.name)
+  description = "Firewall name"
+
+}
+
+output "private_ip_address" {
+  value       = azurerm_firewall.firewall[*].ip_configuration[0].private_ip_address
+  description = "Firewall private IP"
+
+}
+
+output "public_ip_id" {
+  description = "value of public IP ID"
+  value       = azurerm_public_ip.public_ip.*.id
+}
+
+output "public_ip_address" {
+  description = "value of public IP address"
+  value       = azurerm_public_ip.public_ip.*.ip_address
+}
+
+output "firewall_policy_id" {
+  description = "value of firewall policy ID"
+  value       = join("", azurerm_firewall_policy.policy.*.id)
+}
+
+output "prefix_public_ip_id" {
+  description = "value of prefix public IP ID"
+  value       = azurerm_public_ip.prefix_public_ip.*.id
+}
+
+output "prefix_public_ip_address" {
+  description = "value of prefix public IP address"
+  value       = azurerm_public_ip.prefix_public_ip.*.ip_address
+}
+
+output "public_ip_prefix_id" {
+  description = "value of public IP prefix ID"
+  value       = join("", azurerm_public_ip_prefix.pip-prefix.*.id)
 }
