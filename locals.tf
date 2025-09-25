6840b5e838f5c030adb5c2b5fd1b90d5f11a40e2
Ravi Malvia <ravi.malvia@clouddrove.com>
2025-09-25 20:47:39 +0530

    feat: added custom name feature

--- locals.tf
+++ locals.tf
@@ -1,6 +1,7 @@
+
 ##-----------------------------------------------------------------------------
-## Locals
+## Locals declaration for determining the local variables
 ##-----------------------------------------------------------------------------
 locals {
-  label_order = var.label_order
+  name = var.custom_name != null ? var.custom_name : module.labels.id
 }
