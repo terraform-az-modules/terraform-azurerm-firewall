c8d6dbde587ba54bc39964854572f92e5bc0d474
Ravi Malvia <ravi.malvia@clouddrove.com>
2025-10-27 22:45:49 +0530

    fix: Update main.tf with necesary resource and arguments

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 # Standard Tagging Module – Applies standard tags to all resources for traceability
 ##-----------------------------------------------------------------------------
 module "labels" {
-  source          = "terraform-az-modules/tags/azure"
+  source          = "terraform-az-modules/labels/azure"
   version         = "1.0.0"
   name            = var.custom_name == null ? var.name : var.custom_name
   location        = var.location
@@ -15,17 +15,9 @@ module "labels" {
 }
 
 ##-----------------------------------------------------------------------------
-# Local value for naming convention – Determines the name to be used for resources
+# Firewall Public IP Prefix – optional
 ##-----------------------------------------------------------------------------
-locals {
-  public_ip_map = { for n in var.public_ip_names : n => { name = n } }
-}
-
-
-##----------------------------------------------------------------------------- 
-# Firewall Public IP Prefix – Deploys Azure Firewall and its related resources
-##-----------------------------------------------------------------------------
-resource "azurerm_public_ip_prefix" "pip-prefix" {
+resource "azurerm_public_ip_prefix" "pip_prefix" {
   count               = var.enabled && var.firewall_enable && var.public_ip_prefix_enable ? 1 : 0
   name                = format(var.resource_position_prefix ? "public-ip-prefix-%s" : "%s-public-ip-prefix", local.name)
   location            = var.location
@@ -37,46 +29,29 @@ resource "azurerm_public_ip_prefix" "pip-prefix" {
 }
 
 ##-----------------------------------------------------------------------------
-# Primary Public IP – Creates the primary public IP address for Azure Firewall
-##-----------------------------------------------------------------------------
-resource "azurerm_public_ip" "primary_public_ip" {
-  count                = var.enabled && var.firewall_enable ? 1 : 0
-  name                 = format(var.resource_position_prefix ? "ip-%s-%s" : "%s-%s-ip", local.name, var.primary_public_ip_name)
-  location             = var.location
-  resource_group_name  = var.resource_group_name
-  allocation_method    = var.public_ip_allocation_method
-  sku                  = var.public_ip_sku
-  ddos_protection_mode = "VirtualNetworkInherited"
-  tags                 = module.labels.tags
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-##-----------------------------------------------------------------------------
-# Azure Public IP Module – Creates public IP addresses for Azure Firewall
+# Public IPs – one resource, creates all PIPs
 ##-----------------------------------------------------------------------------
 resource "azurerm_public_ip" "public_ip" {
-
-  for_each             = local.public_ip_map
-  name                 = var.public_ip_prefix_enable ? format(var.resource_position_prefix ? "pip-%s-%s" : "%s-%s-pip", local.name, each.key) : format(var.resource_position_prefix ? "ip-%s-%s" : "%s-%s-ip", local.name, each.key)
+  for_each             = toset(var.public_ip_names)
+  name                 = var.public_ip_prefix_enable ? format(var.resource_position_prefix ? "pip-%s-%s" : "%s-%s-pip", local.name, each.value) : format(var.resource_position_prefix ? "ip-%s-%s" : "%s-%s-ip", local.name, each.value)
   location             = var.location
   resource_group_name  = var.resource_group_name
   allocation_method    = var.public_ip_allocation_method
   sku                  = var.public_ip_sku
-  public_ip_prefix_id  = var.public_ip_prefix_enable ? azurerm_public_ip_prefix.pip-prefix[0].id : null
+  public_ip_prefix_id  = var.public_ip_prefix_enable ? azurerm_public_ip_prefix.pip_prefix[0].id : null
   ddos_protection_mode = "VirtualNetworkInherited"
   tags                 = module.labels.tags
+
   lifecycle {
     create_before_destroy = true
   }
 }
 
-
 ##-----------------------------------------------------------------------------
-# Azure Firewall Module – Deploys Azure Firewall with configurations  
+# Azure Firewall
 ##-----------------------------------------------------------------------------
 resource "azurerm_firewall" "firewall" {
+  depends_on          = [azurerm_public_ip.public_ip, azurerm_public_ip_prefix.pip_prefix]
   count               = var.enabled && var.firewall_enable ? 1 : 0
   name                = format(var.resource_position_prefix ? "firewall-%s" : "%s-firewall", local.name)
   location            = var.location
@@ -89,36 +64,38 @@ resource "azurerm_firewall" "firewall" {
   private_ip_ranges   = var.firewall_private_ip_ranges
   dns_servers         = var.dns_servers
 
-  # Primary ip_configuration (requires subnet_id)
+  # Primary ip_configuration (first in the list)
   ip_configuration {
-    name                 = format(var.resource_position_prefix ? "ipconfig-%s-%s" : "%s-%s-ipconfig", local.name, var.primary_public_ip_name)
+    name                 = format(var.resource_position_prefix ? "ipconfig-%s-%s" : "%s-%s-ipconfig", local.name, var.public_ip_names[0])
     subnet_id            = var.subnet_id
-    public_ip_address_id = azurerm_public_ip.primary_public_ip[0].id #azurerm_public_ip.primary_public_ip[0].id
+    public_ip_address_id = azurerm_public_ip.public_ip[var.public_ip_names[0]].id
   }
 
-  # Additional ip_configurations for the remaining PIPs
+  # Additional ip_configurations (skip first one)
   dynamic "ip_configuration" {
-    for_each = { for k, v in azurerm_public_ip.public_ip : k => v if k != var.primary_public_ip_name }
+    for_each = length(var.public_ip_names) > 1 ? toset(slice(var.public_ip_names, 1, length(var.public_ip_names))) : []
     content {
-      name                 = format(var.resource_position_prefix ? "ipconfig-%s-%s" : "%s-%s-ipconfig", local.name, ip_configuration.key)
-      public_ip_address_id = ip_configuration.value.id
-      # subnet_id omitted by design for additional PIPsn
+      name                 = format(var.resource_position_prefix ? "ipconfig-%s-%s" : "%s-%s-ipconfig", local.name, ip_configuration.value)
+      public_ip_address_id = azurerm_public_ip.public_ip[ip_configuration.value].id
+      # subnet_id omitted here!
     }
   }
+
   lifecycle {
     ignore_changes = [tags]
   }
 }
 
-##----------------------------------------------------------------------------- 
-# Azure Firewall Policy – Deploys Azure Firewall Policy with rule collections
+##-----------------------------------------------------------------------------
+# Firewall Policy
 ##-----------------------------------------------------------------------------
 resource "azurerm_firewall_policy" "policy" {
   count               = var.enabled && var.firewall_enable ? 1 : 0
-  name                = format(var.resource_position_prefix ? "firewall-Policy-%s" : "%s-firewall-Policy", local.name)
+  name                = format(var.resource_position_prefix ? "firewall-policy-%s" : "%s-firewall-policy", local.name)
   resource_group_name = var.resource_group_name
   location            = var.location
   sku                 = var.sku_policy
+
   dynamic "identity" {
     for_each = var.identity_type != null && var.sku_policy == "Premium" && var.sku_tier == "Premium" ? [1] : []
     content {
@@ -128,14 +105,16 @@ resource "azurerm_firewall_policy" "policy" {
   }
 }
 
-##----------------------------------------------------------------------------------------------------------
-# Azure firewall Policy Rule Collection Group – Creates App collection groups for Azure Firewall Policy
-##----------------------------------------------------------------------------------------------------------
-resource "azurerm_firewall_policy_rule_collection_group" "app_policy_rule_collection_group" {
+##-----------------------------------------------------------------------------
+# Firewall Policy Rule Collection Groups
+##-----------------------------------------------------------------------------
+resource "azurerm_firewall_policy_rule_collection_group" "app_policy" {
+  depends_on         = [azurerm_firewall_policy.policy]
   count              = var.enabled && var.policy_rule_enabled ? 1 : 0
   name               = var.app_policy_collection_group
   firewall_policy_id = var.firewall_policy_id == null ? join("", azurerm_firewall_policy.policy.*.id) : var.firewall_policy_id
   priority           = 300
+
   dynamic "application_rule_collection" {
     for_each = var.application_rule_collection
     content {
@@ -146,10 +125,11 @@ resource "azurerm_firewall_policy_rule_collection_group" "app_policy_rule_collec
       dynamic "rule" {
         for_each = application_rule_collection.value.rules
         content {
-          name              = lookup(rule.value, "name", null)              # string
-          source_addresses  = lookup(rule.value, "source_addresses", null)  # list # currently the IP of staging rule, needs to be changed
-          source_ip_groups  = lookup(rule.value, "source_ip_groups", null)  # list Specifies a list of source IP groups.
-          destination_fqdns = lookup(rule.value, "destination_fqdns", null) # list of destination IP groups.
+          name              = lookup(rule.value, "name", null)
+          source_addresses  = lookup(rule.value, "source_addresses", null)
+          source_ip_groups  = lookup(rule.value, "source_ip_groups", null)
+          destination_fqdns = lookup(rule.value, "destination_fqdns", null)
+
           dynamic "protocols" {
             for_each = rule.value.protocols
             content {
@@ -163,85 +143,87 @@ resource "azurerm_firewall_policy_rule_collection_group" "app_policy_rule_collec
   }
 }
 
-##---------------------------------------------------------------------------------------------------------------
-# Azure firewall Policy Rule Collection Group – Creates Network rule collection groups for Azure Firewall Policy
-##---------------------------------------------------------------------------------------------------------------
-resource "azurerm_firewall_policy_rule_collection_group" "network_policy_rule_collection_group" {
+resource "azurerm_firewall_policy_rule_collection_group" "network_policy" {
+  depends_on         = [azurerm_firewall_policy.policy]
   count              = var.enabled && var.policy_rule_enabled ? 1 : 0
   name               = var.net_policy_collection_group
   firewall_policy_id = var.firewall_policy_id == null ? join("", azurerm_firewall_policy.policy.*.id) : var.firewall_policy_id
   priority           = 200
+
   dynamic "network_rule_collection" {
     for_each = var.network_rule_collection
     content {
       name     = network_rule_collection.value.name
       priority = network_rule_collection.value.priority
       action   = network_rule_collection.value.action
+
       dynamic "rule" {
         for_each = network_rule_collection.value.rules
         content {
-          name                  = rule.value.name                                   # string
-          protocols             = rule.value.protocols                              # list
-          destination_ports     = rule.value.destination_ports                      # list - Required
-          source_addresses      = lookup(rule.value, "source_addresses", null)      # list
-          source_ip_groups      = lookup(rule.value, "source_ip_groups", null)      # list Specifies a list of source IP groups.
-          destination_addresses = lookup(rule.value, "destination_addresses", null) # list - ["192.168.1.1", "192.168.1.2"]
-          destination_ip_groups = lookup(rule.value, "destination_ip_groups", null) # list of destination IP groups.
-          destination_fqdns     = lookup(rule.value, "destination_fqdns", null)     # list of destination fqdns groups.
+          name                  = rule.value.name
+          protocols             = rule.value.protocols
+          destination_ports     = rule.value.destination_ports
+          source_addresses      = lookup(rule.value, "source_addresses", null)
+          source_ip_groups      = lookup(rule.value, "source_ip_groups", null)
+          destination_addresses = lookup(rule.value, "destination_addresses", null)
+          destination_ip_groups = lookup(rule.value, "destination_ip_groups", null)
+          destination_fqdns     = lookup(rule.value, "destination_fqdns", null)
         }
       }
     }
   }
 }
 
-##------------------------------------------------------------------------------------------------------------
-# Azure firewall Policy Rule Collection Group – Creates NAT rule collection groups for Azure Firewall Policy
-##------------------------------------------------------------------------------------------------------------
-resource "azurerm_firewall_policy_rule_collection_group" "nat_policy_rule_collection_group" {
-  count              = var.enabled && var.dnat_destination_ip && var.policy_rule_enabled ? 1 : 0
+resource "azurerm_firewall_policy_rule_collection_group" "nat_policy" {
+  depends_on         = [azurerm_firewall_policy.policy]
+  count              = var.enabled && var.policy_rule_enabled ? 1 : 0
   name               = var.nat_policy_collection_group
   firewall_policy_id = var.firewall_policy_id == null ? join("", azurerm_firewall_policy.policy.*.id) : var.firewall_policy_id
   priority           = 100
+
   dynamic "nat_rule_collection" {
     for_each = var.nat_rule_collection
     content {
       name     = nat_rule_collection.value.name
       priority = nat_rule_collection.value.priority
       action   = "Dnat"
+
       dynamic "rule" {
         for_each = nat_rule_collection.value.rules
         content {
-          name                = rule.value.name                                 # string
-          protocols           = rule.value.protocols                            # list
-          destination_ports   = rule.value.destination_ports                    # list - Required
-          source_addresses    = lookup(rule.value, "source_addresses", null)    # list
-          destination_address = lookup(rule.value, "destination_address", null) # string
-          translated_address  = lookup(rule.value, "translated_address", null)  # list of translated address.
-          translated_port     = lookup(rule.value, "translated_port", null)     # port
+          name                = rule.value.name
+          protocols           = rule.value.protocols
+          destination_ports   = rule.value.destination_ports
+          source_addresses    = lookup(rule.value, "source_addresses", null)
+          destination_address = lookup(rule.value, "destination_address", null)
+          translated_address  = lookup(rule.value, "translated_address", null)
+          translated_port     = lookup(rule.value, "translated_port", null)
         }
       }
     }
   }
 }
 
-##--------------------------------------------------------------------------------------
-# Azure Firewall Diagnostic Setting – Configures diagnostic settings for Azure Firewall
-##--------------------------------------------------------------------------------------
-resource "azurerm_monitor_diagnostic_setting" "firewall_diagnostic_setting" {
-  count                          = var.enabled && var.enable_diagnostic ? 1 : 0
-  name                           = format(var.resource_position_prefix ? "firewall-diagnostic-log-%s" : "%s-firewall-diagnostic-log", local.name)
+##-----------------------------------------------------------------------------
+# Diagnostics
+##-----------------------------------------------------------------------------
+resource "azurerm_monitor_diagnostic_setting" "firewall_diagnostic" {
+  count                          = var.enabled && var.enable_diagnostic && var.firewall_enable ? 1 : 0
+  name                           = format(var.resource_position_prefix ? "firewall-diagnostic-%s" : "%s-firewall-diagnostic", local.name)
   target_resource_id             = azurerm_firewall.firewall[0].id
   storage_account_id             = var.storage_account_id
   eventhub_name                  = var.eventhub_name
   eventhub_authorization_rule_id = var.eventhub_authorization_rule_id
   log_analytics_workspace_id     = var.log_analytics_workspace_id
   log_analytics_destination_type = var.log_analytics_destination_type
+
   dynamic "enabled_metric" {
     for_each = var.metric_enabled ? ["AllMetrics"] : []
     content {
-      category = enabled_metrics.value
+      category = enabled_metric.value
     }
   }
+
   dynamic "enabled_log" {
     for_each = var.logs
     content {
